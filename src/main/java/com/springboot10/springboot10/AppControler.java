c2d82147dfb9d5a467b4d50b153f33d52b9100a9
pandeysiddhant <pandeysiddhant85@gmail.com>
2022-05-10 16:50:18 +0530

    first commit

---
+++ src/main/java/com/springboot10/springboot10/AppControler.java
@@ -0,0 +1,17 @@
+package com.springboot10.springboot10;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class AppControler {
+
+    @GetMapping("/")
+    public String sayHello(){
+
+        return "Hello this my spring boot app";
+    }
+    
+}
